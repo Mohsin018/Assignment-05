e136244e2fbaf0a105cff11147bd8a20a601c6b7
Mohsin Abdullah Chowdhury <mohsinabd71@gmail.com>
2025-08-29 11:15:51 +0600

    copy button added

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 // // Heart icon responsive
 
-  const counterEl = document.getElementById('heart-counter');
-  const hearts = document.querySelectorAll('.heart');
 
-  hearts.forEach(heart => {
-    heart.addEventListener('click', function () {
-      // Increase navbar counter
-      let count = parseInt(counterEl.textContent);
-      counterEl.textContent = count + 1;
+const counter = document.getElementById('heart-counter');
+const heartParent = document.getElementById('heart-parent');
 
+heartParent.addEventListener('click', function (e) {
+  if (e.target.classList.contains('heart')) {
+    let count = parseInt(counter.innerText);
+    counter.innerText = count + 1;
+  }
+});
+
+// ================= Copy Hotline Number ==================
+ const copyCounter = document.getElementById("copy-counter");
+  const copyButtons = document.querySelectorAll(".copy-btn");
+
+  copyButtons.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const card = btn.closest(".card"); // find the card
+      const serviceNumber = card.querySelector(".hotline-number").innerText;
+
+      // Copy to clipboard
+      navigator.clipboard.writeText(serviceNumber).then(() => {
+        alert(`Number copied: ${serviceNumber}`);
+
+        // Increase counter
+        let count = parseInt(copyCounter.innerText);
+        copyCounter.innerText = count + 1;
+      });
     });
   });
 
@@ -38,13 +57,48 @@
     // Show alert
     alert("Calling " + serviceName.innerText + " at " + serviceNumber.innerText);
 
-    // Add to history
-    let li = document.createElement("li");
-    li.innerText = serviceName.innerText + "\n" + serviceNumber.innerText;
-    callHistory.appendChild(li);
+
+
+
+
+
+    // Add history section
+let li = document.createElement("li");
+
+// full text including time
+let now = new Date();
+let time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+li.innerHTML = `
+  <div>
+    ${serviceName.innerText}<br>
+    ${serviceNumber.innerText}
+  </div>
+  <span style="font-size:12px; color:gray;">${time}</span>
+`;
+               
+
+
+li.style.display = "flex";
+li.style.justifyContent = "space-between";
+li.style.alignItems = "center"; 
+li.style.backgroundColor = "#dddddd";
+li.style.borderRadius = "10px";
+li.style.padding = "8px";
+li.style.marginBottom = "6px";
+li.style.listStyle = "none";
+
+callHistory.appendChild(li);
   });
+
+
+
 
   // Clear History Button Action
   clearBtn.addEventListener("click", function () {
     callHistory.innerHTML = "";
   });
+
+
+
+  
